530f582aae52056d23e5569676c30b0fabdf61ce
Joe Lim <jlim3131@gmail.com>
2017-03-18 23:57:58 -0700

    update cities with lat lng

--- MemeApi/queries.js
+++ MemeApi/queries.js
@@ -1,43 +1,62 @@
 var promise = require('bluebird');
+var axios = require('axios');
 
 var options = {
-  promiseLib: promise
+    promiseLib: promise
 };
 
 var pgp = require('pg-promise')(options);
 var connectionString = 'postgres://root@nwmeme.westus.cloudapp.azure.com:26257/nwmeme';
 var db = pgp(connectionString);
 
+function getAxiosForCity(cityName) {
+  return axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${cityName}.json?access_token=pk.eyJ1IjoiYW5keXR1bmciLCJhIjoiY2owZnJ6eXY4MDJlbTJxc2F6OW81cnpzcSJ9.CwCldAdWdHqo90qFuK_WFA`);
+}
+
 function getAllCities(req, res, next) {
-  db.any('SELECT * FROM cities')
-    .then(function (data) {
-      res.status(200)
-        .json({
-          status: 'success',
-          data: data
+    db.any('SELECT * FROM cities')
+        .then(function (data) {
+            var arr = data.map(function (i) {
+                return getAxiosForCity(i.cityName);
+            });
+            axios.all(arr)
+                .then(function (axiosResults) {
+                    var temp = axiosResults.map(r => r.data);
+                    for (var i=0; i<temp.length; i++) {
+                        data[i].location = {
+                            lat: temp[i].features[0].center[1],
+                            lng: temp[i].features[0].center[0]
+                        }
+                    }
+                    res.status(200)
+                        .json({
+                            status: 'success',
+                            data: data
+                        });
+                });
+            
+        })
+        .catch(function (err) {
+            return next(err);
         });
-    })
-    .catch(function (err) {
-      return next(err);
-    });
 }
 
 function getTweetsByCityName(req, res, next) {
-  var cityName = req.params.cityName;
-  db.any(`SELECT * FROM tweets INNER JOIN cities ON tweets.cityId = cities.cityID WHERE cities.cityName = '${cityName}' ORDER BY tweets.createdAt LIMIT 5`)
-    .then(function (data) {
-      res.status(200)
-        .json({
-          status: 'success',
-          data: data
+    var cityName = req.params.cityName;
+    db.any(`SELECT * FROM tweets INNER JOIN cities ON tweets.cityId = cities.cityID WHERE cities.cityName = '${cityName}' ORDER BY tweets.createdAt LIMIT 5`)
+        .then(function (data) {
+            res.status(200)
+                .json({
+                    status: 'success',
+                    data: data
+                });
+        })
+        .catch(function (err) {
+            return next(err);
         });
-    })
-    .catch(function (err) {
-      return next(err);
-    });
 }
 
 module.exports = {
-  getAllCities: getAllCities,
-  getTweetsByCityName: getTweetsByCityName,
+    getAllCities: getAllCities,
+    getTweetsByCityName: getTweetsByCityName,
 };
