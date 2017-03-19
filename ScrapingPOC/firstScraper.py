2da79186fc48933c2298d677a89317e4fd0ad4f4
seanrt <seanrtohidi@gmail.com>
2017-03-19 00:11:34 -0400

    The timestamp for each tweet is now grabbed and passed to the database along with all the other information.

--- ScrapingPOC/firstScraper.py
+++ ScrapingPOC/firstScraper.py
@@ -2,6 +2,7 @@ import re
 import psycopg2
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 HASHTAGS_TO_SEARCH = ['dank','memes','pepe']
 CITY_TO_SEARCH = 'Vancouver'
@@ -37,24 +38,26 @@ def getHTMLSoupFromCityAndHashtags(hashtags, city):
     return soup
 
 class TweetData():
-    def __init__(self, tweet, url, replyCount, retweetCount, likeCount):
+    def __init__(self, tweet, url, replyCount, retweetCount, likeCount, timestamp):
         self.tweet = tweet
         self.url = url
         self.replyCount = replyCount if replyCount != '' else '0'
         self.retweetCount = retweetCount if retweetCount != '' else '0'
         self.likeCount = likeCount if likeCount != '' else '0'
+        self.timestamp = str(timestamp)
 
     def __str__(self):
-        return self.tweet+'\n'+self.url+'\n'+'Reply count: '+self.replyCount+'\n'+'Retweet count: '+self.retweetCount+'\n'+'Like count: '+self.likeCount
+        return self.timestamp+'\n'+self.tweet+'\n'+self.url+'\n'+'Reply count: '+self.replyCount+'\n'+'Retweet count: '+self.retweetCount+'\n'+'Like count: '+self.likeCount
 
 def getTweetDataFromSoup(soup):
     tweetData = []
     for div in soup.findAll('div', class_='content'):
         tweet, url = cleanUpRawTweets(div.find('p', class_='tweet-text').text)
+        timestamp = datetime.fromtimestamp(int(div.find('span', class_='_timestamp')['data-time']))
         replyCount = div.find('div', class_='ProfileTweet-action--reply').find('div', class_='IconTextContainer').find('span', class_='ProfileTweet-actionCountForPresentation').text
         retweetCount = div.find('div', class_='ProfileTweet-action--retweet').find('div', class_='IconTextContainer').find('span', class_='ProfileTweet-actionCountForPresentation').text
         likeCount = div.find('div', class_='ProfileTweet-action--favorite').find('div', class_='IconTextContainer').find('span', class_='ProfileTweet-actionCountForPresentation').text
-        tweetData.append(TweetData(tweet, url, replyCount, retweetCount, likeCount))
+        tweetData.append(TweetData(tweet, url, replyCount, retweetCount, likeCount, timestamp))
     return tweetData
 
 def updateTweetsInDatabase():
@@ -74,7 +77,7 @@ def updateTweetsInDatabase():
         print('------'+city+'------')
         for tweet in tweetData:
             print(tweet)
-            databaseCursor.execute("INSERT INTO nwmeme.tweets (cityId, tweet, imageUrl, retweetCount, likesCount, repliesCount, tweetedTime) SELECT city.cityId, '"+tweet.tweet+"', '"+tweet.url+"', "+tweet.retweetCount+", "+tweet.likeCount+", "+tweet.replyCount+", 'a' FROM nwmeme.cities city WHERE cityName = '"+city+"'")
+            databaseCursor.execute("INSERT INTO nwmeme.tweets (cityId, tweet, imageUrl, retweetCount, likesCount, repliesCount, tweetedTime) SELECT city.cityId, '"+tweet.tweet+"', '"+tweet.url+"', "+tweet.retweetCount+", "+tweet.likeCount+", "+tweet.replyCount+", '"+tweet.timestamp+"' FROM nwmeme.cities city WHERE cityName = '"+city+"'")
 
     databaseCursor.close()
     conn.close()
