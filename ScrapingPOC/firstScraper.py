789366c9b1599ff5643ed37d379244200b5f2d65
seanrt <seanrtohidi@gmail.com>
2017-03-18 22:01:05 -0400

    The script grabs tweet data including the image url, likescount, repliescount, and retweetscount. The data is written to the database. Tweets are found for cities listed in the database.

--- ScrapingPOC/firstScraper.py
+++ ScrapingPOC/firstScraper.py
@@ -1,9 +1,9 @@
 import re
+import psycopg2
 import requests
 from bs4 import BeautifulSoup
 
-# HASHTAGS_TO_SEARCH = ['dank','memes','pepe']
-HASHTAGS_TO_SEARCH = ['Memes']
+HASHTAGS_TO_SEARCH = ['dank','memes','pepe']
 CITY_TO_SEARCH = 'Vancouver'
 DISTANCE_TO_SEARCH_IN_MILES = '500'
 
@@ -19,19 +19,67 @@ def removeURLsFromTweets(tweet):
     return tweet
 
 def cleanUpRawTweets(tweet):
+    url = re.search(r"pic.twitter.com\S+", tweet)
+    url = url.group(0) if url != None else 'No url'
+    tweet = re.sub(r"pic.twitter.com\S+", '', tweet)
     tweet = removeURLsFromTweets(tweet)
     tweet = tweet.replace('\n',' ')
+    tweet = tweet.replace("'", '')
     # tweet = tweet.encode('utf-8', 'ignore').decode('utf-8')
     # tweet = tweet.encode('utf-8')
     tweet = tweet.encode('ascii', 'ignore').decode('ascii')
-    return tweet
+    return tweet, url
 
-query = hashtagListToURLstringQuery(HASHTAGS_TO_SEARCH)
-twitterURL = 'https://twitter.com/search?vertical=default&q='+query+'%20near%3A%22'+CITY_TO_SEARCH+'%22%20within%3A'+DISTANCE_TO_SEARCH_IN_MILES+'mi'
+def getHTMLSoupFromCityAndHashtags(hashtags, city):
+    twitterURL = 'https://twitter.com/search?vertical=default&q='+hashtags+'%20near%3A%22'+city+'%22%20within%3A'+DISTANCE_TO_SEARCH_IN_MILES+'mi'
+    requestHTML = requests.get(twitterURL)
+    soup = BeautifulSoup(requestHTML.text, 'html.parser')
+    return soup
 
-r = requests.get(twitterURL)
-soup = BeautifulSoup(r.text, 'html.parser')
+class TweetData():
+    def __init__(self, tweet, url, replyCount, retweetCount, likeCount):
+        self.tweet = tweet
+        self.url = url
+        self.replyCount = replyCount if replyCount != '' else '0'
+        self.retweetCount = retweetCount if retweetCount != '' else '0'
+        self.likeCount = likeCount if likeCount != '' else '0'
 
-tweets = [cleanUpRawTweets(p.text) for p in soup.findAll('p', class_='tweet-text')]
-for tweet in tweets:
-    print(tweet)
+    def __str__(self):
+        return self.tweet+'\n'+self.url+'\n'+'Reply count: '+self.replyCount+'\n'+'Retweet count: '+self.retweetCount+'\n'+'Like count: '+self.likeCount
+
+def getTweetDataFromSoup(soup):
+    tweetData = []
+    for div in soup.findAll('div', class_='content'):
+        tweet, url = cleanUpRawTweets(div.find('p', class_='tweet-text').text)
+        replyCount = div.find('div', class_='ProfileTweet-action--reply').find('div', class_='IconTextContainer').find('span', class_='ProfileTweet-actionCountForPresentation').text
+        retweetCount = div.find('div', class_='ProfileTweet-action--retweet').find('div', class_='IconTextContainer').find('span', class_='ProfileTweet-actionCountForPresentation').text
+        likeCount = div.find('div', class_='ProfileTweet-action--favorite').find('div', class_='IconTextContainer').find('span', class_='ProfileTweet-actionCountForPresentation').text
+        tweetData.append(TweetData(tweet, url, replyCount, retweetCount, likeCount))
+    return tweetData
+
+def updateTweetsInDatabase():
+    conn = psycopg2.connect(database='bank', user='root', host='nwmeme.westus.cloudapp.azure.com', port=26257)
+    conn.set_session(autocommit=True)
+    databaseCursor = conn.cursor()
+    databaseCursor.execute('SELECT cityName FROM nwmeme.cities')
+    citiesDB = databaseCursor.fetchall()
+    cities = []
+    for city in citiesDB:
+        cities.append([str(cell) for cell in city][0])
+
+    hashtagsToQuery = hashtagListToURLstringQuery(HASHTAGS_TO_SEARCH)
+    for city in cities:
+        soup = getHTMLSoupFromCityAndHashtags(hashtagsToQuery, 'Vancouver')
+        tweetData = getTweetDataFromSoup(soup)
+        print('------'+city+'------')
+        for tweet in tweetData:
+            print(tweet)
+            databaseCursor.execute("INSERT INTO nwmeme.tweets (cityId, tweet, imageUrl, retweetCount, likesCount, repliesCount, tweetedTime) SELECT city.cityId, '"+tweet.tweet+"', '"+tweet.url+"', "+tweet.retweetCount+", "+tweet.likeCount+", "+tweet.replyCount+", 'a' FROM nwmeme.cities city WHERE cityName = '"+city+"'")
+
+    databaseCursor.close()
+    conn.close()
+
+def updateMemeDatabase():
+    pass
+
+updateTweetsInDatabase()
